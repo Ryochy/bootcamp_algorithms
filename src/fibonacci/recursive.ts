0d4c6f68b09721a7c0354dfddb728683ee243222
= <ryochi.my@gmail.com>
2023-04-18 02:02:53 +0900

    clear

--- src/fibonacci/recursive.ts
+++ src/fibonacci/recursive.ts
@@ -1,5 +1,5 @@
 export const fibonacciRecursive = (n: number): number => {
-    if(n <= 1) return n;
+  if (n <= 1) return n;
 
-    return fibonacciRecursive(n-2) + fibonacciRecursive(n-1);
+  return fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1);
 };
