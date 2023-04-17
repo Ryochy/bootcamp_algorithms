41a35f2473ff6ba5f0f4e8e38165847ecd59a1da
= <ryochi.my@gmail.com>
2023-04-18 01:27:15 +0900

    clear

--- src/fibonacci/loop.ts
+++ src/fibonacci/loop.ts
@@ -1,2 +1,14 @@
 export const fibonacciLoop = (n: number): number => {
+
+    if(n <= 1) return n;
+
+    let prev2 = 0;
+    let prev1 = 1;
+    let result = 1; // 初期値を1に設定
+    for(let i=2; i<=n; i++){
+        result = prev2 + prev1;
+        prev2 = prev1;
+        prev1 = result
+    }
+    return result; // forループの外に移動
 };
